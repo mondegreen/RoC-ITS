dde3b8e573d4ea188d94f7e8680e6aa161354283
mondegreen <54181197+mondegreen@users.noreply.github.com>
2020-10-01 16:30:24 -0400

    Update treeDists.pl
    Updated to the correct version

--- treeDists.pl
+++ treeDists.pl
@@ -1,31 +1,38 @@
 use Bio::TreeIO;
 use Bio::Tree::TreeFunctionsI;
 use FileUtil;
-
-### usage looks like this:
-### find readData/ -name \*subs.best.dnd | perl treeDists.pl - > suspect.trees
-### output is input to extractValidSubSeqs.pl
+use BioStats;
 
 my $list = shift;
+my $cutoff = shift;
+$cutoff = 0.05 unless defined($cutoff);
+
 my $lfh = FileUtil::openFileHandle($list);
 while (<$lfh>) {
   chomp;
   my $file = $_;
   $n++;
   
+  my @dists;
+  my $cnt;
   print STDERR "$n\n" unless $n % 100;
   my $treeio = Bio::TreeIO->new(-format => 'newick', -file => $file);
   while ( $tree = $treeio->next_tree ) {
     my @taxa = $tree->get_leaf_nodes;
+    $cnt = @taxa;
     for (my $j = 1; $j < @taxa; $j++) {
       my $b = $taxa[$j]->id;
       push @dists,$tree->distance(-nodes => [$taxa[0], $taxa[$j]]);
     }
+    for (my $j = 2; $j < @taxa; $j++) {
+      my $b = $taxa[$j]->id;
+      push @dists,$tree->distance(-nodes => [$taxa[1], $taxa[$j]]);
+    }
   }
-  my $n = @dists;
-  for (my $i = 0; $i < $n; $i++) { push @dists,0;}
-  my $l = join ",",@dists;
-  print $n,"\n",$l,"\n";
-  
+  my $mean = BioStats::getMean(\@dists);
+  my $std = BioStats::getMeanStandardDeviation(\@dists,$mean);
+  my $x = @taxa;
+  my $l = join " ",$file,$mean,$std,$cnt,"xx",@dists;
+  print $l,"\n" if $std > $cutoff;
 }
 $lfh->close();
